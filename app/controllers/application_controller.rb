3047ccc9951be4147b5aa885925a9c213dc52dd8
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-12-25 01:22:24 +0800

    Include SessionsHelper module into Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-  	render html: "Sample application under construction"
-  end
+  include SessionsHelper
 end
