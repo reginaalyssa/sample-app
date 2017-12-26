70fd604003f7f4f5e18ae99d7908c749d3fe6f8f
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-12-26 13:58:46 +0800

    Log user in after signing up for an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # Also equivalent to user_url(@user)
     else
