542827d26d8ee292358fc117a0e921107cab8ece
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-12-26 19:59:56 +0800

    Revert redefining new token and digest methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,16 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  class << self
-    # Returns the hash digest of the given string.
-    def digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-    # Returns a random token.
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
   end
 
   # Remembers a use rin the database for use in persistent sessions.
