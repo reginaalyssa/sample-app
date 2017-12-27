db811c588f6e01d7fadd72a205b45043e79149e6
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-12-27 21:56:47 +0800

    Add test to make sure friendly forwarding happens only the first time and not on subsequent logins

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -51,4 +51,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  test "friendly forwarding only forwards to given URL the first time" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+    delete logout_path
+    log_in_as(@user)
+    assert_redirected_to @user
+    assert_nil session[:forwarding_url]
+  end
 end
