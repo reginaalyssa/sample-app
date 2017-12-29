57f33c11c8c7ac056c5f3aa8aab847118df27780
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-12-30 00:05:52 +0800

    Update test to display active users only in users index

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,16 +7,20 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index as admin including pagination and delete links" do
+  test "index as admin including pagination and delete links and displays active users only" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
       end
     end
     assert_difference 'User.count', -1 do
