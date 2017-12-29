0299f0798a624edf471e03f8864333f34a1e9b9a
Regina Alyssa <reginaalyssa01809@gmail.com>
2017-12-30 00:18:31 +0800

    Add integration test for user profile for activated and non-activated accounts

---
+++ test/integration/users_show_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class UsersShowTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @admin     = users(:michael)
+    @activated_user = users(:archer)
+    @non_activated_user = users(:malory)
+  end
+
+  test "should redirect if user account not activated" do
+    log_in_as(@admin)
+    get user_path(@non_activated_user)
+    assert_redirected_to root_url
+  end
+
+  test "should show user profile if user account is activated" do
+    log_in_as(@admin)
+    get user_path(@activated_user)
+    assert_redirected_to @activated_user
+  end
+end
